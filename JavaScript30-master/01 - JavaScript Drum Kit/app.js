097fc4c9f24a02bccc27d8d7293ae34ef7ebfac2
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-10-18 10:11:32 +0900

    TypeScriptやめる

--- JavaScript30-master/01 - JavaScript Drum Kit/app.js
+++ JavaScript30-master/01 - JavaScript Drum Kit/app.js
@@ -1,6 +1,9 @@
-"use strict";
 window.addEventListener('keydown', function (e) {
-    console.log(e.code);
-    var audio = document.querySelector("audio[data-key=\"" + e.code + "\"]");
-    console.log(audio);
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  // audioに定義されていないキーを押した場合は処理をしない
+  // console.log(audio);
+  if (!audio) return;
+
+	audio.currentTime = 0;
+  audio.play();
 });
--- JavaScript30-master/01 - JavaScript Drum Kit/app.ts
+++
@@ -1,5 +0,0 @@
-window.addEventListener('keydown', function (e) {
-	console.log(e.code);
-  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-  console.log(audio);
-});
