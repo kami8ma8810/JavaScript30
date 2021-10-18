c43d174c55fd0871d7f0228698bf2da5bc82f2e5
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-10-18 10:37:42 +0900

    01完成

--- JavaScript30-master/01 - JavaScript Drum Kit/app.js
+++ JavaScript30-master/01 - JavaScript Drum Kit/app.js
@@ -1,9 +1,26 @@
-window.addEventListener('keydown', function (e) {
-  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-  // audioに定義されていないキーを押した場合は処理をしない
-  // console.log(audio);
-  if (!audio) return;
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; //トランスフォーム以外の処理は行わない
+  e.target.classList.remove('playing');
+}
 
-	audio.currentTime = 0;
-  audio.play();
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  const key = document.querySelector(`.key[data-key="${e.code}"]`);
+
+  if (!audio) return; // audioに定義されていないキーを押した場合は処理をしない
+  key.classList.add('playing');
+  audio.currentTime = 0; //即時にキーを押したときの遅延をなくす
+  audio.play(); //HTMLMediaElement.play()
+  /*
+	参照 https://developer.mozilla.org/ja/docs/Web/API/HTMLMediaElement/play
+	 */
+}
+
+// すべてのキーに対してtransitionendのタイミングでremoveTransitonの処理をするイベントを設定
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => {
+  key.addEventListener('transitionend', removeTransition);
 });
+
+// 実行
+window.addEventListener('keydown', playSound);
