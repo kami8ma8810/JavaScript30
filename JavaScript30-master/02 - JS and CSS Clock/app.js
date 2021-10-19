ed5a1ed6310d4da80c7440d0e0b4f3362630cee3
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-10-19 09:34:36 +0900

    02実装完了

---
+++ JavaScript30-master/02 - JS and CSS Clock/app.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  // 秒針の処理
+  const seconds = now.getSeconds();
+  const secondDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  // 分針の処理
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  // 時針の処理
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+setInterval(setDate, 1000);
